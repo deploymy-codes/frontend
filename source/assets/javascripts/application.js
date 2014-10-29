904716d35e81003fc829cb9d843a1e54ecc873ae
Kevin <kevin@disneur.me>
2014-10-29 21:10:03 +0000

    Add sign in/sign up template pages, resolves #1

---
+++ source/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require "jquery"
+//= require "foundation"
+//= require "deploy_my_codes"
+//= require_self
---
+++ source/assets/javascripts/deploy_my_codes.js
@@ -0,0 +1,5 @@
+//= require "angular"
+//= require "angular-resource"
+//= require_self
+
+angular.module('deployMyCodes', ['ngResource', 'ngRoute']);
