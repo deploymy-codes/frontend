734174ab707f31e6564ca47cbd2e03e3eb8f22f4
Kevin <kevin@disneur.me>
2014-11-27 20:19:49 +0000

    Move hard coded string to config

--- app/javascripts/config.js
+++ app/javascripts/config.js
@@ -1,12 +1,19 @@
 require.register('config', function(exports, require, module){
   module.exports = function() {
+    var _ = require('underscore');
     var environment = /* @echo GULP_ENV */ || 'development';
 
     return {
       development: {
+        api: {
+          tokenName: 'api_key',
+        },
         github: {
-          client_id: 'da3c2c42130e5391041f',
-          api_authentication_url: 'http://localhost:9292/auth?provider=github'
+          clientId: 'da3c2c42130e5391041f',
+          apiAuthenticationURL: 'http://localhost:9292/auth?provider=github'
+        },
+        session: {
+          prefix: 'deploy_my_codes'
         }
       },
       production:  {},
--- app/javascripts/deploy_my_codes/config/oauth.js
+++ app/javascripts/deploy_my_codes/config/oauth.js
@@ -1,10 +1,10 @@
 require.register('deploy_my_codes/config/oauth', function(exports, require, module){
   module.exports = function($authProvider, config) {
-    $authProvider.tokenName     = 'api_key';
-    $authProvider.tokenPrefix   = 'deploy_my_codes';
+    $authProvider.tokenName   = config.api.tokenName;
+    $authProvider.tokenPrefix = config.session.prefix;
     $authProvider.github({
-      clientId: config.github.client_id,
-      url:      config.github.api_authentication_url
+      clientId: config.github.clientId,
+      url:      config.github.apiAuthenticationURL
     });
   };
 });
